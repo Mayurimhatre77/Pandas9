cacbb5e887a48ed0aa3b17a92a9bcd7c140bdea3
Mayuri Mhatre <mayurimhatre77@gmail.com>
2025-03-18 19:48:03 -0500

    Done PR

---
+++ Problem1.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
+    first_login_date = activity.groupby('player_id')['event_date'].min().reset_index()
+    first_login_date.columns = ['player_id', 'first_login']
+    return first_login_date 
